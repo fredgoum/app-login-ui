3ff163e1e89da7e95d564b6e9763a85fdcd20a21
fredgoum <alfredgoumou@gmail.com>
2021-02-17 22:26:04 +0100

    navigation bar

---
+++ frontend/src/components/Home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- Navigation -->
+    <navigation></navigation>
+  </div>
+</template>
+
+<script>
+  import Navigation from '../Navigation/Navigation';
+
+  export default {
+    components: {
+      Navigation,
+    }
+  }
+</script>
---
+++ frontend/src/components/Navigation/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar app color="#72CFBF" dark>
+    <!-- Logo -->
+    <div class="d-flex align-center">
+      <div>Ikena</div>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <!-- User Avatar -->
+    <v-avatar>
+      <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="avatar">
+    </v-avatar>
+  </v-app-bar>
+</template>
